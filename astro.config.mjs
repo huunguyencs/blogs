4b6099dda578db95e0762debafacc22fe6c82efe
Nguyen Van Huu <160694623+huunguyenkite@users.noreply.github.com>
2026-03-28 21:48:20 +0700

    update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourblog.com',
+  site: "https://huunguyencs.github.io/blogs/",
+  base: "/blogs",
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
     },
   },
 });
